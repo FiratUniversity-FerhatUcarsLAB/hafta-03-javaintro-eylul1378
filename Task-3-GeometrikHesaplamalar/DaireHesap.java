ebc16e50e564adb3f4b886f8971296bd35a119c3
nihal eylül <21nihaleylulil@gmail.com>
2025-10-23 17:38:10 +0300

    Update DaireHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DaireHesap.java
@@ -0,0 +1,17 @@
+package GÖREV3;
+
+public class Dairealanı {
+    public static void main (String[] args) {
+
+    double r=5.5;
+    double pi=3.1415;
+
+    double alan = pi*r*r;
+    double cevre= 2*pi*r;
+
+    System.out.println("yaricap:" + r);
+    System.out.println("alan: " + alan);
+    System.out.println("çevre: " + cevre);
+
+    }
+    }
