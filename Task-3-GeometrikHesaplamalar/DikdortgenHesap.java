c651b64585c43729e34414d06a5f8d49e2351423
nihal eylül <21nihaleylulil@gmail.com>
2025-10-23 17:38:36 +0300

    Update DikdortgenHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,18 @@
+package GÖREV3;
+
+public class Dikdortgenalanı {
+    public static void main (String[] args) {
+
+    double kisaKenar=4.5;
+    double uzunKenar=7.9;
+
+    double alan = kisaKenar*uzunKenar;
+    double cevre= 2*(kisaKenar+uzunKenar);
+
+    System.out.println("uzun kenar:" + uzunKenar);
+    System.out.println("kisa kenar:" + kisaKenar);
+    System.out.println("alan: " + alan);
+    System.out.println("çevre: " + cevre);
+
+    }
+    }
