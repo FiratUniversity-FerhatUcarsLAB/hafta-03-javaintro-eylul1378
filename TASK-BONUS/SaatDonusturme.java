9f6f9458da937649f0d96cbf1a75c42e8fb17619
nihal eylül <21nihaleylulil@gmail.com>
2025-10-23 17:59:36 +0300

    Update SaatDonusturme.java

---
+++ TASK-BONUS/SaatDonusturme.java
@@ -0,0 +1,19 @@
+package BONUS;
+
+public class Bonus1{
+    public static void main(String[] args) {
+
+    int saniye1 = 7854;
+    System.out.println(saniye1 +  "saniye ="  + (saniye1 / 3600) +  "saat"  +  ((saniye1 % 3600) / 60) +  "dakika"  + (saniye1 % 60) +  "saniye" );
+
+    int saniye2 = 125000;
+    System.out.println(saniye2 +  "saniye ="  + (saniye2 / 3600) +  "saat"  +  ((saniye2 % 3600) / 60) +  "dakika"  + (saniye2 % 60) +  "saniye" );
+
+    int saniye3 = 85412;
+    System.out.println(saniye3 +  "saniye ="  + (saniye3 / 3600) +  "saat"  + ((saniye3 % 3600) / 60) +  "dakika"  +  (saniye3 % 60) +  "saniye" );
+
+    int saniye4 = 987654;
+    System.out.println(saniye4 +  "saniye ="  + (saniye4 / 3600) +  "saat"  + ((saniye4 % 3600) / 60) +  "dakika"  + (saniye4 % 60) +  "saniye" );
+
+}
+}
