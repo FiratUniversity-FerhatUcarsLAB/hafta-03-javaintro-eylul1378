18279f25ca85a9533d220f9cc3abdb8d82bb68f7
nihal eylül <21nihaleylulil@gmail.com>
2025-10-23 17:20:48 +0300

    Create UcMesaj class with welcome messages

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,10 @@
+package GÖREV1;
+
+public class Ucmesaj {
+    public static void main(String[] args) {
+        
+        System.out.println("Java'ya Hoş Geldiniz!");
+        System.out.println("Programlama Eğlencilidir!");
+        System.out.println("Bilgisayar Bilimine Hoş Geldiniz!");    
+    } 
+    }
