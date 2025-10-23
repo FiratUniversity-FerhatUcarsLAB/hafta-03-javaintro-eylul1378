1c0ccf414f99e27609c50e86049c5ddf5911e818
nihal eylül <21nihaleylulil@gmail.com>
2025-10-23 17:44:52 +0300

    Update AsciiArtHarf.java

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,15 @@
+package GÖREV4;
+
+public class ASCIIart {
+    public static void main(String[] args) {
+        
+        System.out.println("  //////////////" );
+        System.out.println("  ///           " );
+        System.out.println("  ///           " );
+        System.out.println("  //////////////" );
+        System.out.println("  ///           " );
+        System.out.println("  ///           " ); 
+        System.out.println("  //////////////" );
+    
+}
+}
