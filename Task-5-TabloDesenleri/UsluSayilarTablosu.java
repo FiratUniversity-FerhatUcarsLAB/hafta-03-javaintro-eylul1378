6e1c6059e47163912b42b72ceff92991aea31021
nihal eylül <21nihaleylulil@gmail.com>
2025-10-23 17:52:51 +0300

    Implement power table for numbers 2, 4, and 6

---
+++ Task-5-TabloDesenleri/UsluSayilarTablosu.java
@@ -0,0 +1,20 @@
+package GÖREV5;
+
+public class Uslusayılar {
+    public static void main(String[] args) {
+         
+        System.out.println("a       a^2      a^3");
+        System.out.println("-----------------------");
+
+        int a1 = 2;
+        System.out.println(a1 + "       " + (a1 * a1) + "       " + (a1 * a1 * a1));
+
+        int a2 = 4;
+        System.out.println(a2 + "       " + (a2 * a2) + "       " + (a2 * a2 * a2));
+
+
+        int a3 = 6;
+        System.out.println(a3 + "       " + (a3 * a3) + "       " + (a3 * a3 * a3));
+   
+    }
+    }
