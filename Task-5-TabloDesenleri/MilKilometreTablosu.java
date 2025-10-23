29d696078d286f52102d4992c210f47c2f529da5
nihal eylül <21nihaleylulil@gmail.com>
2025-10-23 17:51:48 +0300

    Update MilKilometreTablosu.java

---
+++ Task-5-TabloDesenleri/MilKilometreTablosu.java
@@ -0,0 +1,25 @@
+package GÖREV5;
+
+public class Donusumtablosu {
+    public static void main(String[] args) {
+
+        System.out.println("Mil         Kilometre");
+        System.out.println("---------------------");
+
+        double mil1 = 1;
+        System.out.println(mil1 + "       " + (mil1 * 1.609));
+
+        double mil2 = 5;
+        System.out.println(mil2 + "       " + (mil2 * 1.609));
+
+        double mil3 = 10;
+        System.out.println(mil3 + "      " + (mil3 * 1.609));
+
+        double mil4 = 20;
+        System.out.println(mil4 + "      " + (mil4 * 1.609));
+
+        double mil5 = 50;
+        System.out.println(mil5 + "      " + (mil5 * 1.609));
+     
+    }
+    }
