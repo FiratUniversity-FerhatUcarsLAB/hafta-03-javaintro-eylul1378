67dd9230acda78dbda448fb7f1ae629c514c8f8c
nihal eylül <21nihaleylulil@gmail.com>
2025-10-23 17:45:38 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,21 @@
+package GÖREV4;
+
+public class İsimkartı {
+    public static void main (String[] args) {
+        
+        String ad = "Nihal Eylül";
+        String soyad = "İL";
+        String ogrencino = "250541001";
+        String bolum = "Yazılım Mühendisliği";
+
+        System.out.println("=====================");
+        System.out.println("OGRENCI BILGILERI");
+        System.out.println("=======================");
+        System.out.println("Ad :" + ad);
+        System.out.println("Soyad : " + soyad);
+        System.out.println("No : "+ ogrencino);
+        System.out.println("Bolum : " + bolum);
+        System.out.println("=====================");
+
+    }
+    } 
