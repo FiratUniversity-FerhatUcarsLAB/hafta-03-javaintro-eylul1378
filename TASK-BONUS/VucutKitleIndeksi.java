71b62c470bc1678cc1c9c5f04d8a6abcae254f9a
nihal eylül <21nihaleylulil@gmail.com>
2025-10-23 18:00:07 +0300

    Update VucutKitleIndeksi.java

---
+++ TASK-BONUS/VucutKitleIndeksi.java
@@ -0,0 +1,19 @@
+package BONUS;
+
+public class Bonus2{
+    public static void main(String[] args) {
+
+    double boy1 = 1.70;
+    double kilo1 = 60;    
+    System.out.println("BMI-1=" + (kilo1/(boy1*boy1)));
+
+    double boy2=1.54;
+    double kilo2=56;
+    System.out.println("BMI-2=" + (kilo2/(boy2*boy2)));
+
+    double boy3=1.98;
+    double kilo3=105;
+    System.out.println("BMI-3=" + (kilo3/(boy3*boy3)));
+
+}
+}
