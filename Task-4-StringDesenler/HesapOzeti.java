5628241bf899ca6b75776c6173e36cff4262eb82
nihal eylül <21nihaleylulil@gmail.com>
2025-10-23 17:45:17 +0300

    Add Hesapozeti class for product summary

---
+++ Task-4-StringDesenler/HesapOzeti.java
@@ -0,0 +1,26 @@
+package GÖREV4;
+
+public class Hesapozeti {
+    public static void main(String[] args) {
+
+    System.out.println( "           Urun adeti     Urun fiyati      Toplam tutar");
+    System.out.println("--------------------------------------------------------");
+
+    int elmaadeti=5;
+    int elmafiyati=3;
+    int elmatoplami=elmaadeti*elmafiyati;
+
+    int armutadeti=6;
+    int armutfiyati=4;
+    int armuttoplami=armutadeti*armutfiyati;
+
+    int muzadeti=8;
+    int muzfiyati=2;
+    int muztoplami=muzadeti*muzfiyati; 
+
+    System.out.println("Elma          "+elmaadeti+"               "+elmafiyati+"                "+elmatoplami);
+    System.out.println("Armut         "+armutadeti+"               "+armutfiyati+"                "+armuttoplami);
+    System.out.println("Muz           "+muzadeti+"               "+muzfiyati+"                "+muztoplami);
+     
+    }
+    }
