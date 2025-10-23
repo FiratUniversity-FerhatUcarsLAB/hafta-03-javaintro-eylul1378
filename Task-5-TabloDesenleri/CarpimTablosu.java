b729f0b7233fc5b3f88017d3db5e46a163de5ab9
nihal eylül <21nihaleylulil@gmail.com>
2025-10-23 17:51:23 +0300

    Update CarpimTablosu.java

---
+++ Task-5-TabloDesenleri/CarpimTablosu.java
@@ -0,0 +1,40 @@
+package GÖREV5;
+
+public class Carpımtablosu {
+    public static void main(String[] args) {
+
+        System.out.println("Carpımtablosu");
+        System.out.println("--------------");
+
+        int sayi1 = 1;
+        System.out.println(sayi1 + " x 2 = " + (sayi1 * 2));
+
+        int sayi2 = 2;
+        System.out.println(sayi2 + " x 2 = " + (sayi2 * 2));
+
+        int sayi3 = 3;
+        System.out.println(sayi3 + " x 2 = " + (sayi3 * 2));
+
+        int sayi4 = 4; 
+        System.out.println(sayi4 + " x 2 = " + (sayi4 * 2));
+
+        int sayi5 = 5;
+        System.out.println(sayi5 + " x 2 = " + (sayi5 * 2 ));
+
+        int sayi6 = 6;
+        System.out.println(sayi6 + " x 2 = " + (sayi6 * 2));
+
+        int sayi7 = 7;
+        System.out.println(sayi7 + " x 2 = " + (sayi7 * 2));
+
+        int sayi8 = 8;
+        System.out.println(sayi8 + " x 2 = " + (sayi8 * 2));
+
+        int sayi9 = 9;
+        System.out.println(sayi9 + " x 2 = " + (sayi9 * 2));
+
+        int sayi10 = 10;
+        System.out.println(sayi10 + " x 2 = " + (sayi10 * 2));
+    
+  }
+  }
