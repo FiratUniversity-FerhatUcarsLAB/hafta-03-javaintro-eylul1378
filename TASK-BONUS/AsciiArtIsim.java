209e50d692c05b85cc07ba61949bde7d61e4132b
nihal eylül <21nihaleylulil@gmail.com>
2025-10-23 17:58:27 +0300

    Update AsciiArtIsim.java

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,13 @@
+package BONUS;
+public class Bonus4{
+
+        public static void main(String[] args) {
+        
+        System.out.println(" //////  /    /  /          / /    /     ");
+        System.out.println(" //       / /    /        /    /   /      ");
+        System.out.println(" //////    /     /        /    /   /      ");
+        System.out.println(" //        /     /        /    /   /      ");
+        System.out.println(" //////    /     //////    ////    /////  ");
+
+    }
+    } 
